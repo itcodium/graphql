0faf24ee340f8d81f626aa43359b6a45f7e05226
itcodium <p.haddad.p@gmail.com>
2020-01-18 16:17:31 -0300

    Graphql apollo-server, se actualiza puerto (v2)

--- config/GraphQL/graphql.js
+++ config/GraphQL/graphql.js
@@ -8,13 +8,15 @@ module.exports = function (app, bodyParser) {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    introspection: true
+    introspection: true,
+    playground: true,
   });
   server.applyMiddleware({ app });
-  var port = process.env.PORT || 4000;
+  //var port = process.env.PORT || 4000;
 
+  /*
   app.listen({ port: port }, () => {
     console.log(`1(🚀) Server ready at http://localhost:${ port }${ server.graphqlPath }`);
-  });
+  });*/
 
 }
