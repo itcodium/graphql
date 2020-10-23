b457d85b8689e06858fde6a922118f7986f8185e
pablo <itcodium>
2020-10-23 12:35:05 -0300

    2020-10-23 Se agrega el frontend react

---
+++ public/precache-manifest.3534af268e0cc3131a3a8c1f6d0375be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "32f90dbe9ba57fddca78d74f9e72a5dd",
+    "url": "/static/media/logo.32f90dbe.png"
+  },
+  {
+    "revision": "a62a83f37b05e303205142ff51d7a990",
+    "url": "/static/media/es.a62a83f3.svg"
+  },
+  {
+    "revision": "db68882cbf4beac3f3ee3923f1a91b82",
+    "url": "/static/media/en.db68882c.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "96982a451063181bb646",
+    "url": "/static/js/main.96982a45.chunk.js"
+  },
+  {
+    "revision": "ad9113f47c0f7a5ec1c4",
+    "url": "/static/js/1.ad9113f4.chunk.js"
+  },
+  {
+    "revision": "96982a451063181bb646",
+    "url": "/static/css/main.cc228d4b.chunk.css"
+  },
+  {
+    "revision": "2cdccb762faabd4db5a16d7251ee7f91",
+    "url": "/index.html"
+  }
+];
--- push.bat
+++ push.bat
@@ -1,3 +1,3 @@
 git add .
-git commit -m "2020-10-23 Se completan todos los endpoints del timer"
+git commit -m "2020-10-23 Se agrega el frontend react"
 git push
