971a6d73bbb48a3227e3df9d0fd1ec50b34da82c
pablo <itcodium>
2020-11-13 19:21:07 -0300

    2020-10-23 Se agrega el frontend react

--- app/controllers/books.js
+++ app/controllers/books.js
@@ -5,87 +5,88 @@ var async = require('async')
 
 
 exports.getAll = function (req, res) {
-    var order = {};
-    if (req.query.column) {
-        try {
+    try {
+        var order = {};
+        if (req.query.column) {
             var sort = req.query.order == "1" ? "1" : "-1";
             order = JSON.parse("{\"" + req.query.column + "\":\"" + sort + "\"}");
         }
-        catch (err) {
-            res.status(500).jsonp({ "message": err.message });
-        }
-    }
-    var perPage = parseInt(req.query.pagesize);
-    var page = (parseInt(req.query.page) - 1) * perPage;
+        var perPage = parseInt(req.query.pagesize);
+        var page = (parseInt(req.query.page) - 1) * perPage;
 
-    Books.find({})
-        .sort(order)
-        .skip(page)
-        .limit(perPage)
-        .exec(function (err, data) {
-            Books.countDocuments().exec(function (err, count) {
-                if (err) {
-                    res.status(500).jsonp({ error: err.message });
-                }
-                res.jsonp(
-                    {
-                        response: {
-                            code: 0,
-                            result: data,
-                            current: page,
-                            recordsTotal: count,
-                            recordsFiltered: count
-                        }
+        Books.find({})
+            .sort(order)
+            .skip(page)
+            .limit(perPage)
+            .exec(function (err, data) {
+                Books.countDocuments().exec(function (err, count) {
+                    if (err) {
+                        return res.status(400).jsonp({ error: err.message });
                     }
-                );
+                    res.jsonp({
+                        result: data,
+                        current: page,
+                        recordsTotal: count,
+                        recordsFiltered: count
+                    });
+                })
             })
-        })
+    } catch (err) {
+        return res.status(500).json({ error: err.message });
+    }
 };
 
 exports.item = function (req, res, next, id) {
     Books.findById(id, function (err, item) {
         if (err) { return next(err); }
-        if (!item) { return res.json({ status: "error", errors: "No se encontro el registro." }); }
+        if (!item) { return res.status(400).json({ error: "No se encontro el registro." }); }
         req.item = item;
         return next();
     })
+
 }
 exports.getById = function (req, res) {
-    res.jsonp(req.item);
+    try {
+        res.jsonp(req.item);
+    } catch (err) {
+        return res.status(500).json({ error: err.message });
+    }
 }
 exports.create = function (req, res) {
     try {
         var item = new Books(req.body);
         item.save(function (err, book) {
             if (err) {
-                return res.status(500).json({success: false, error: err.message});
+                return res.status(400).json({ error: err.message });
             }
-            res.json({ status: "ok", message: "Book successfully added!", book });
+            res.jsonp({ book });
         });
     }
     catch (err) {
-        return res.status(500).json({success: false, error: err.message});
+        return res.status(500).json({ error: err.message });
     }
 }
 exports.update = function (req, res) {
-    Books.findById({ _id: req.params.id }, (err, book) => {
-        if (err) {
-            return res.send({ status: "error", errors: err.message });
-        }
-        Object.assign(book, req.body).save((err, book) => {
+    try {
+        Object.assign(req.item, req.body).save((err, item) => {
             if (err) {
-                return res.send({ status: "error", errors: err.message });
+                return res.status(400).send({ error: err.message });
             }
             res.json({ message: 'Book updated!', book });
         });
-    });
-
+    } catch (err) {
+        return res.status(500).json({ error: err.message });
+    }
 }
 exports.delete = function (req, res) {
-    Books.remove({ _id: req.params.id }, (err, result) => {
-        if (err) {
-            return res.send({ status: "error", errors: err.message });
-        }
-        res.json({ status: "ok", message: "Books successfully deleted!" });
-    });
+    try {
+        Books.remove({ _id: req.params.id }, (err, result) => {
+            if (err) {
+                return res.status(400).send({ error: err.message });
+            }
+            res.json({ message: "Books successfully deleted!" });
+        });
+    } catch (err) {
+        return res.status(500).json({ error: err.message });
+    }
 }
